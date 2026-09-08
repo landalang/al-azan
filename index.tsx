1adeef33573c040014b3cdd88a33e5b75a11e562
landalang <cheikhmbackediop@proton.me>
2026-09-08 21:27:08 +0000

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,56 +1,265 @@
-import {LogBox, AppRegistry} from 'react-native';
-import {App} from '@/app';
-import {BaseComponent} from '@/base_component';
-import {bootstrap} from '@/bootstrap';
-import {handleDemoCommands} from '@/modules/activity';
-import {onUpdateScreenWidgetRequested} from '@/modules/screen_widget';
-import {setupNotifeeHandlers} from '@/notifee';
-import {FullscreenAlarm} from '@/screens/fullscreen_alarm';
-import {setUpdateWidgetsAlarms} from '@/tasks/set_update_widgets_alarms';
-import {updateWidgets} from '@/tasks/update_widgets';
+import {t} from '@lingui/macro';
+import type {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {
+  FlatList,
+  Stack,
+  Button,
+  Modal,
+  FormControl,
+  Input,
+  WarningOutlineIcon,
+  Text,
+  IStackProps,
+  ScrollView,
+} from 'native-base';
+import {useCallback, useEffect, useState} from 'react';
+import {ToastAndroid} from 'react-native';
+import {pickSingle} from 'react-native-document-picker';
+import {useStore} from 'zustand';
+import {shallow} from 'zustand/shallow';
+import {Prayer, PrayersInOrder} from '@/adhan';
+import {AdhanEntry} from '@/assets/adhan_entries';
+import PrayerAdhan from '@/components/prayer_adhan';
+import {SafeArea} from '@/components/safe_area';
+import MediaPlayer, {
+  AudioEntry,
+  PlaybackState,
+  usePlaybackState,
+} from '@/modules/media_player';
+import {RootStackParamList} from '@/navigation/types';
 
-if (__DEV__) {
-  const ignoreWarns = [
-    'UNSAFE_componentWillReceiveProps',
-    'UNSAFE_componentWillMount',
-    'If you do not provide children, you must specify an aria-label for accessibility',
-    'In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app',
-    '[Reanimated] Tried to modify key `current` of an object which has been already passed to a worklet',
-  ];
+import {AdhanListItem} from '@/screens/settings_adhan/adhan_list_item';
+import {play, stop, destroy} from '@/services/play_sound';
+import {settings} from '@/store/settings';
 
-  const warn = console.warn;
-  console.warn = (...arg) => {
-    for (const warning of ignoreWarns) {
-      if (arg[0].startsWith(warning)) {
-        return;
+type AdhanSettingsProps = NativeStackScreenProps<
+  RootStackParamList,
+  'AdhanSettings'
+>;
+
+export function AdhanSettings(props: IStackProps & AdhanSettingsProps) {
+  const {navigation} = props;
+
+  const [playingAdhanEntry, setPlayingAdhanEntry] = useState<
+    AdhanEntry | undefined
+  >();
+
+  const {
+    SELECTED_ADHAN_ENTRIES,
+    setSelectedAdhan,
+    resetPrayerAdhans,
+    SAVED_ADHAN_AUDIO_ENTRIES,
+    ADVANCED_CUSTOM_ADHAN,
+  } = useStore(
+    settings,
+    s => ({
+      SELECTED_ADHAN_ENTRIES: s.SELECTED_ADHAN_ENTRIES,
+      setSelectedAdhan: s.setSelectedAdhan,
+      SAVED_ADHAN_AUDIO_ENTRIES: s.SAVED_ADHAN_AUDIO_ENTRIES,
+      ADVANCED_CUSTOM_ADHAN: s.ADVANCED_CUSTOM_ADHAN,
+      resetPrayerAdhans: s.resetPrayerAdhans,
+    }),
+    shallow,
+  );
+
+  const setSelectedFajrAdhan = useCallback(
+    (item: AdhanEntry | undefined) => setSelectedAdhan(Prayer.Fajr, item),
+    [setSelectedAdhan],
+  );
+
+  const [selectedFilePath, setSelectedFilePath] = useState<string>();
+  const [newAdhanName, setNewAdhanName] = useState<string | null>();
+  const playerState = usePlaybackState();
+
+  useEffect(() => {
+    const sub = MediaPlayer.addEventListener('error', () => {
+      ToastAndroid.show(
+        t`An error occured while playing the current track`,
+        ToastAndroid.SHORT,
+      );
+    });
+    return () => {
+      sub.remove();
+    };
+  });
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('beforeRemove', () => {
+      destroy();
+    });
+
+    return unsubscribe;
+  });
+
+  if (ADVANCED_CUSTOM_ADHAN) {
+    return (
+      <SafeArea>
+        <ScrollView>
+          <Stack flex={1} px="4" py="2" {...props}>
+            <Button
+              variant="outline"
+              mb="3"
+              onPress={() => navigation.navigate('WeeklyAdhanSettings')}>
+              {t`Different Adhan Every Day (Fajr)`}
+            </Button>
+            {PrayersInOrder.map(prayer => (
+              <PrayerAdhan
+                prayer={prayer}
+                key={prayer}
+                selectedEntry={SELECTED_ADHAN_ENTRIES[prayer] as AudioEntry}
+                setSelectedAdhan={setSelectedAdhan}
+              />
+            ))}
+          </Stack>
+        </ScrollView>
+      </SafeArea>
+    );
+  }
+
+  const playAdhanEntry = async (item: AdhanEntry) => {
+    try {
+      if (
+        playingAdhanEntry?.id === item.id &&
+        playerState === PlaybackState.started
+      ) {
+        await stop();
+      } else {
+        setPlayingAdhanEntry(item);
+        await stop();
+        if (item.filepath) {
+          await play({
+            audioEntry: item as AudioEntry,
+            preferExternalDevice: true,
+          });
+        }
       }
+    } catch (e) {
+      console.error(e);
     }
-    warn(...arg);
   };
-  LogBox.ignoreLogs(ignoreWarns);
+
+  const onAdhanItemPressed = (item: AdhanEntry) => {
+    if (item.filepath) {
+      resetPrayerAdhans(item);
+    } else {
+      ToastAndroid.show(
+        t`Adhan audio file is not downloaded yet`,
+        ToastAndroid.SHORT,
+      );
+    }
+  };
+
+  const onAddCustomAdhanPressed = () => {
+    pickSingle({
+      copyTo: 'documentDirectory',
+      type: [
+        'audio/mpeg',
+        'audio/ogg',
+        'application/ogg',
+        'audio/opus',
+        'audio/x-wav',
+        'audio/webm',
+        'audio/m4a',
+      ],
+    })
+      .then(val => {
+        if (val.copyError) {
+          console.error(val.copyError);
+          ToastAndroid.show(val.copyError, ToastAndroid.LONG);
+        } else {
+          setNewAdhanName(val.name);
+          setSelectedFilePath(val.fileCopyUri!);
+        }
+      })
+      .catch(() => {});
+  };
+
+  const renderItem = ({item}: {item: AdhanEntry}) => {
+    return (
+      <AdhanListItem
+        onAdhanSelected={onAdhanItemPressed}
+        onFajrAdhanSelected={setSelectedFajrAdhan}
+        key={item.id}
+        item={item}
+        playAdhanEntry={playAdhanEntry}
+        playerState={playerState}
+        selected_item_id={SELECTED_ADHAN_ENTRIES.default.id}
+        selected_fajr_item_id={SELECTED_ADHAN_ENTRIES.fajr?.id}
+        playing_item_id={playingAdhanEntry?.id}
+      />
+    );
+  };
+
+  const onNewAdhanCancel = () => {
+    setSelectedFilePath(undefined);
+    setNewAdhanName(null);
+  };
+
+  const onNewAdhanAdd = () => {
+    if (!newAdhanName) return;
+    settings.getState().saveAdhanEntry({
+      id: 'adhan_' + Date.now().toString(),
+      canDelete: true,
+      label: newAdhanName,
+      filepath: selectedFilePath,
+    });
+    onNewAdhanCancel();
+  };
+
+  return (
+    <SafeArea>
+      <Stack flex={1} pt="4" {...props}>
+        <Button
+          variant="outline"
+          mx="1"
+          mb="2"
+          onPress={() => navigation.navigate('WeeklyAdhanSettings')}>
+          {t`Different Adhan Every Day (Fajr)`}
+        </Button>
+        <FlatList
+          flex={1}
+          data={SAVED_ADHAN_AUDIO_ENTRIES}
+          renderItem={renderItem}
+          extraData={[playerState]}
+        />
+        <Button
+          onPress={onAddCustomAdhanPressed}
+          m="1">{t`Add Custom Adhan`}</Button>
+
+        <Modal
+          size="full"
+          isOpen={!!selectedFilePath}
+          onClose={onNewAdhanCancel}>
+          <Modal.Content borderRadius={0}>
+            <Modal.CloseButton />
+            <Modal.Header>{t`Add Custom Adhan`}</Modal.Header>
+            <Modal.Body>
+              <FormControl isInvalid={!newAdhanName}>
+                <FormControl.Label>{t`Name`}</FormControl.Label>
+                <Input
+                  value={newAdhanName || ''}
+                  placeholder={t`Name`}
+                  onChangeText={str => setNewAdhanName(str)}
+                />
+                <FormControl.ErrorMessage
+                  leftIcon={<WarningOutlineIcon color="yellow.300" />}>
+                  <Text color="yellow.400">
+                    {t`Selecting a name is required`}
+                  </Text>
+                </FormControl.ErrorMessage>
+              </FormControl>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button mx="2" colorScheme="coolGray" onPress={onNewAdhanCancel}>
+                {t`Cancel`}
+              </Button>
+              <Button colorScheme="coolGray" onPress={onNewAdhanAdd}>
+                {t`Add`}
+              </Button>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+      </Stack>
+    </SafeArea>
+  );
 }
-
-setupNotifeeHandlers();
-
-onUpdateScreenWidgetRequested(async () => {
-  bootstrap();
-  await Promise.all([setUpdateWidgetsAlarms(), updateWidgets()]);
-});
-
-handleDemoCommands();
-
-AppRegistry.registerRunnable('main-app', async initialProps => {
-  bootstrap();
-  AppRegistry.registerComponent('main-app', () =>
-    BaseComponent.bind(this, App),
-  );
-  AppRegistry.runApplication('main-app', initialProps);
-});
-
-AppRegistry.registerRunnable('fs-alarm', async initialProps => {
-  bootstrap();
-  AppRegistry.registerComponent('fs-alarm', () =>
-    BaseComponent.bind(this, FullscreenAlarm),
-  );
-  AppRegistry.runApplication('fs-alarm', initialProps);
-});
